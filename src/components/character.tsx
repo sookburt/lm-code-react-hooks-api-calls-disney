a67e189ea7657dcc59f80142878924fd362aac72
Sue Burt <sookburt@gmail.com>
2022-05-03 19:13:56 +0100

    fix non existant problem with image url for assignment lol

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,9 +1,21 @@
 import { DisneyCharacter } from "../disney_character"
 
-// for our props we can reuse the DisneyCharacter interface
-// - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
-  
+const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => {
+
+  // Define a default in case the character doesn't have an image
+  let imageSrc = "https://picsum.photos/300/200/?blur";
+  if (character.imageUrl) {
+    // API seems to include extra path for images so here we strip it off to fetch raw image	
+    const endIndex = character.imageUrl.indexOf('/revision'); // now returning -1
+    if(endIndex > 0) {
+      imageSrc = character.imageUrl.substring(0, endIndex);
+    }
+    else {
+      imageSrc = character.imageUrl;
+    }
+  }
+
+  return (
     <article className="character-item">
 
       <h2>{character.name}</h2>
@@ -12,10 +24,10 @@ const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) =>
         Add to Favourites
       </div>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+      <img className="character-item__img" src={imageSrc} alt={character.name} />
 
     </article>
-  
+  )
+} 
 
-
-export default Character
+export default Character;
